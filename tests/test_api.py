aed680cb0393fc4e339a9949551cee8c4d85bc8a
yiliu30 <yi4.liu@intel.com>
2024-12-06 07:38:43 -0500

    add log info
    Signed-off-by: yiliu30 <yi4.liu@intel.com>

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,3 +2,23 @@ def test_api():
     from torchutils.eval import eval_wikitext2
     from torchutils.bench import bench_module, bench_more
     from torchutils import freeze_seed
+
+
+def test_log_info():
+    from torchutils import log_info
+
+    log_info()
+    import logging
+
+    logger = logging.getLogger(__file__)
+    test_msg = "This is a test message!!!!!!!!!!!"
+    logger.info(test_msg)
+
+
+def test_log_info2(caplog):
+    import logging
+    from torchutils import log_info
+
+    with caplog.at_level(logging.INFO):
+        log_info()
+        assert "Set logging level to INFO successfully" in caplog.text
