05a384d186e933890d4c11604ce6f6a4034f6170
yiliu30 <yi4.liu@intel.com>
2025-11-17 03:40:32 -0500

    update
    Signed-off-by: yiliu30 <yi4.liu@intel.com>

--- examples/parse_metadata.py
+++ examples/parse_metadata.py
@@ -5,6 +5,11 @@ from safetensors import safe_open
 
 def parse_safetensors(folder: str, output_csv: str):
     rows = []
+    if output_csv == "":
+        # parse model base name from folder
+        model_base_name = os.path.basename(os.path.normpath(folder))
+        output_csv = f"{model_base_name}_safetensors_info.csv"
+        print(f"No output file specified. Using default: {output_csv}")
 
     for root, _, files in os.walk(folder):
         for file in files:
@@ -19,19 +24,28 @@ def parse_safetensors(folder: str, output_csv: str):
                             tensor = f.get_tensor(tensor_name)
                             dtype = str(tensor.dtype)
                             shape = list(tensor.shape)
-
+                            tensor_min, tensor_max = "N/A", "N/A"
+                            try:
+                                tensor_min, tensor_max = tensor.min().item(), tensor.max().item()
+                            except:
+                                pass
+                            tensor_min, tensor_max = str(tensor_min), str(tensor_max)
                             rows.append({
                                 "file": os.path.relpath(file_path, folder),
                                 "tensor": tensor_name,
                                 "dtype": dtype,
-                                "shape": str(shape)
+                                "shape": str(shape),
+                                "min": tensor_min,
+                                "max": tensor_max
                             })
                 except Exception as e:
                     print(f"  Error reading {file_path}: {e}")
 
     print(f"\nSaving results to {output_csv} ...")
     with open(output_csv, "w", newline="") as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=["file", "tensor", "dtype", "shape"])
+        writer = csv.DictWriter(csvfile, fieldnames=["file", "tensor", "dtype", "shape"
+                                                     , "min", "max"
+                                                     ])
         writer.writeheader()
         writer.writerows(rows)
     print("Done ✅")
@@ -40,7 +54,7 @@ def parse_safetensors(folder: str, output_csv: str):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Parse tensor dtype/shape from safetensors files.")
     parser.add_argument("folder", help="Path to the folder containing safetensors files")
-    parser.add_argument("-o", "--output", default="safetensors_info.csv",
+    parser.add_argument("-o", "--output", default="",
                         help="Output CSV file name (default: safetensors_info.csv)")
     args = parser.parse_args()
 
